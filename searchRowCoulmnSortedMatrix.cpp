0e7d719777b824880594e4adcaeaed1cdfe330f2
KasatNiraj <53268215+KasatNiraj@users.noreply.github.com>
2020-06-22 21:22:44 +0530

    Create searchRowCoulmnSortedMatrix.cpp

---
+++ searchRowCoulmnSortedMatrix.cpp
@@ -0,0 +1,26 @@
+/*  Function to search x in the input matrix
+*   every row and column is sorted in increasing order
+*   n, m: rows and columns of matrix
+*   x: element to check presence in matrix
+*   mat[][]: input matrix
+*/
+int search( int n,int m, int x, int mat[SIZE][SIZE])
+{
+    if (n == 0) 
+        return 0;
+    //checking if element is out of boundary elements
+    if(x<mat[0][0] || x>mat[n-1][m-1])
+        return 0;
+    int i=0;
+    int j=m-1;
+    while(i<n && j>=0){
+        if(mat[i][j]==x)
+            return 1;
+        else if(mat[i][j]>x) //if element in current column
+                  j--;      //is greater then search in j--
+        else               //if element in current row is smaller
+             i++;         //then search in i++
+    }
+    return 0;
+    
+}
